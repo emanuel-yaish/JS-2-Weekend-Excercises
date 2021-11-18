58f9510f97aafcf64645b69b0f5e40cf593a7e94
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-18 20:24:35 +0200

    submit 8 find the perimter of a rectangle

---
+++ Ex8-Find-Perimeter-Rectangle.js
@@ -0,0 +1,7 @@
+function findPerimeter(lenght, width) {
+  return lenght * 2 + width * 2;
+}
+
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
