c07f85e570d86000fdd5f9ecf7483fd6039a6f21
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-18 18:04:28 +0200

    submit 6.2 counting duplicates

--- Ex6.2-Counting-Duplicates.js
+++ Ex6.2-Counting-Duplicates.js
@@ -1,5 +1,19 @@
-function countingDuplicates(str) {}
+function countingDuplicates(str) {
+  const occurrence = {};
+  let res = 0;
+  const chars = str.toLowerCase().split("");
 
-console.log(accum(""));
-console.log(accum(""));
-console.log(accum(""));
+  chars.forEach((char) => {
+    occurrence[char] = occurrence[char] + 1 || 1;
+    if (occurrence[char] === 2) res++;
+  });
+  return res;
+}
+
+console.log(countingDuplicates("abcde"));
+console.log(countingDuplicates("aabbcde"));
+console.log(countingDuplicates("aabBcde"));
+console.log(countingDuplicates("indivisibility"));
+console.log(countingDuplicates("Indivisibilities"));
+console.log(countingDuplicates("aA11"));
+console.log(countingDuplicates("ABBA"));
